bc3bae2fdc7907dcfe804dcdd0ef9c9c4000a4a1
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-14 17:48:59 -0300

    Add events

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ const subtract = document.querySelector(".subtract");
 const reset = document.querySelector(".reset");
 const add = document.querySelector(".add");
 
+add.addEventListener('click', () => {
+    count.innerHTML++;
+})
+
+subtract.addEventListener('click', () => {
+    count.innerHTML--;
+})
+
+reset.addEventListener('click', () => {
+    count.innerHTML = 0;
+})
+
+
