bfc53c12b6c4f90ab5dae0ba18f730ffac41f490
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-14 18:15:38 -0300

    Add events

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
+//const subtract = document.querySelector(".subtract");
+//const reset = document.querySelector(".reset");
+//const add = document.querySelector(".add");
 const count = document.querySelector(".count");
-const subtract = document.querySelector(".subtract");
-const reset = document.querySelector(".reset");
-const add = document.querySelector(".add");
+const buttons = document.querySelector(".buttons")
 
-add.addEventListener('click', () => {
+buttons.addEventListener('click', event => {    
+    if(event.target.classList.contains('add'))
+    count.innerHTML++;
+
+    if(event.target.classList.contains('subtract'))
+    count.innerHTML--;
+
+    if(event.target.classList.contains('reset'))
+    count.innerHTML = 0;
+});
+
+
+
+
+
+
+/* add.addEventListener('click', () => {
     count.innerHTML++;
 })
 
@@ -14,5 +31,5 @@ subtract.addEventListener('click', () => {
 reset.addEventListener('click', () => {
     count.innerHTML = 0;
 })
-
+ */
 
