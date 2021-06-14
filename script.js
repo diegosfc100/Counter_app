8086373d7e7c9168fba678132630b4fd322a1f16
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-14 16:13:01 -0300

    Select buttons

---
+++ script.js
@@ -0,0 +1,5 @@
+const count = document.querySelector(".count");
+const subtract = document.querySelector(".subtract");
+const reset = document.querySelector(".reset");
+const add = document.querySelector(".add");
+
