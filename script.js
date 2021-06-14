5fd8f7a86e3631ab1e9fa62df3441026b82b295f
diegosfc100 <diego.sfc100@hotmail.com>
2021-06-14 18:34:28 -0300

    Add counter style function

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ const buttons = document.querySelector(".buttons")
 buttons.addEventListener('click', event => {    
     if(event.target.classList.contains('add'))
     count.innerHTML++;
+    setColor();
 
     if(event.target.classList.contains('subtract'))
     count.innerHTML--;
+    setColor();
 
     if(event.target.classList.contains('reset'))
     count.innerHTML = 0;
+    setColor();
+
 });
 
+const setColor = () => {
+    if(count.innerHTML > 0) count.style.color = "green"; 
 
+    else if (count.innerHTML < 0) count.style.color = "tomato"
+
+    else count.style.color = "#fff"    
+}
+    /* else if(count.innerHTML < 0) count.style.color("tomato")
+
+    else count.style.color = (#fff) */
 
 
 
